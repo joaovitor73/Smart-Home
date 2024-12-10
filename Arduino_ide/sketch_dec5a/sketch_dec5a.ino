a03e8359950a1546b87e53c22aa3a3878e2dd1c3
joaovitor73 <joao.vieira.712@ufrn.edu.br>
2024-12-10 13:57:33 -0300

    atualizando

--- Arduino_ide/sketch_dec5a/sketch_dec5a.ino
+++ Arduino_ide/sketch_dec5a/sketch_dec5a.ino
@@ -2,13 +2,14 @@
 #include <WiFi.h>
 #include <FirebaseClient.h>
 #include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 #define WIFI_SSID "A"
 #define WIFI_PASSWORD "12345678"
-#define API_KEY "AIzaSyAM7rxPXK3n8g69MNEXZzsLKOXn9cwARWk"
+#define API_KEY "AIzaSyB2hyLRaBR3ZVqmnHYkvXN1IvZV_NJusFc"
 #define USER_EMAIL "adm@gmail.com"
 #define USER_PASSWORD "admin123"
-#define DATABASE_URL "https://smart-home-2e603-default-rtdb.firebaseio.com/"
+#define DATABASE_URL "https://smart-home-876c0-default-rtdb.firebaseio.com/"
 
 FirebaseApp app;
 DefaultNetwork network;
@@ -22,8 +23,33 @@ AsyncClient aClient(ssl_client, getNetwork(network));
 
 unsigned long tmo = 0; 
 
-const byte led = 2;
+const byte ledSala = 2;
+const byte ledCozinha = 18;
+const byte ledQuarto = 19;
+const byte ledBanheiro = 21;
+const int pinoPIR = 15;
+int cachePresenca = 0;
+enum Comodo {
+    SALA,
+    QUARTO,
+    COZINHA,
+    BANHEIRO, 
+    DESCONHECIDO
+};
 
+enum Sensor{
+  PRESENCA,
+  UMIDADE,
+  LUZ, 
+  TEMPERATURA,
+  LED, 
+  LCD,
+  MOTOR,
+  DISTANCIA,
+  LED_RGB
+};
+
+int cont = 0;
 void setup(){
 
     Serial.begin(115200);
@@ -48,7 +74,11 @@ void setup(){
     app.getApp<RealtimeDatabase>(Database);
     Database.url(DATABASE_URL);
 
-    pinMode(led, OUTPUT); 
+    pinMode(ledSala, OUTPUT); 
+    pinMode(ledCozinha, OUTPUT); 
+    pinMode(ledQuarto, OUTPUT); 
+    pinMode(ledBanheiro, OUTPUT);
+    pinMode(pinoPIR, INPUT); 
 }
 
 void loop(){
@@ -56,25 +86,19 @@ void loop(){
     app.loop();
     Database.loop();
 
-    if (app.ready())
-    {
-      
-       tmo = millis();
-       
-       // User code can be put here
-       int v1 = Database.get<int>(aClient, "led/state");
-       Serial.println(v1);
-       digitalWrite(led, v1); 
+    if (app.ready()){  
+      //configureJson();
+      getDados();
+      // Database.get(aClient, "/smart_home/json", asyncCB);
     }
 }
-
 void asyncCB(AsyncResult &aResult)
 {
-  
 
     if (aResult.isEvent())
     {
-        Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+       Firebase.printf("Event task: %s, msg: %s, code: %d\n", aResult.uid().c_str(), aResult.appEvent().message().c_str(), aResult.appEvent().code());
+      // getDados();
     }
 
     if (aResult.isDebug())
@@ -92,3 +116,230 @@ void asyncCB(AsyncResult &aResult)
         Firebase.printf("task: %s, payload: %s\n", aResult.uid().c_str(), aResult.c_str());
     }
 }
+
+void getDados(){
+  String jsonData = Database.get<String>(aClient, "/smart_home/json");
+  if (aClient.lastError().code() == 0) {
+    Serial.println("ok");
+    Serial.println("Retrieved JSON:");
+    Serial.println(jsonData);
+
+    DynamicJsonDocument doc(2048);
+    DeserializationError error = deserializeJson(doc, jsonData);
+    if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return;
+    }
+    getEstates(jsonData, doc);
+  }else{
+    Serial.print("Failed to get JSON: ");
+    Serial.println(aClient.lastError().message());
+  }
+}
+
+void getEstates(String jsonData, DynamicJsonDocument doc){
+  JsonObject comodos = doc["comodos"];
+  for (JsonPair comodo : comodos) {
+    const char* nomeComodo = comodo.key().c_str(); 
+    JsonObject sensores = comodo.value()["sensores"];
+
+    Serial.printf("Cômodo: %s\n", nomeComodo);
+    for (JsonPair sensor : sensores) {
+      const char* tipoSensor = sensor.key().c_str(); 
+      JsonObject sensorData = sensor.value().as<JsonObject>();
+      swhitchComfortable(nomeComodo, sensorData["valor"], sensorData["id"], tipoSensor);
+    }
+  }
+}
+
+int verificaUltimoValor(JsonObject sensorData){
+  int ultimoValor = 0;
+  if (sensorData.containsKey("estado")) {
+    ultimoValor = sensorData["estado"];
+  }
+  if (sensorData.containsKey("velocidade")) {
+    ultimoValor = sensorData["velocidade"];
+  }
+  if (sensorData.containsKey("valor")) {
+    ultimoValor = sensorData["valor"];
+  }
+  if (sensorData.containsKey("intensidade")) {
+    ultimoValor = sensorData["intensidade"];
+  }
+  return ultimoValor;
+}
+
+void swhitchComfortable(const char* nomeComodo, int ultimoValor, int id, const char* tipoSensor){
+  Comodo comodo = getComodoEnum(nomeComodo);
+  Sensor sensor = getSensoresEnum(tipoSensor);
+  switch(comodo){
+    case 3: updateBanheiro(ultimoValor, sensor, id, tipoSensor, nomeComodo);
+      break;
+    case 2: updateCozinha(ultimoValor, sensor, id, tipoSensor, nomeComodo);
+      break;
+    case 1: updateQuarto(ultimoValor, sensor, id, tipoSensor, nomeComodo);
+      break;
+    case 0: updateSala(ultimoValor, sensor, id, tipoSensor, nomeComodo);
+      break;
+  }
+}
+
+void updateBanheiro(int valor, Sensor sensor,int id, const char* tipoSensor, const char* nomeComodo){
+   switch(sensor){ 
+    case 4: digitalWrite(ledBanheiro, valor);
+      break;
+    case 2: 
+      break;
+  }
+}
+
+void updateCozinha(int valor, Sensor sensor, int id, const char* tipoSensor, const char* nomeComodo){
+   switch(sensor){
+    case 4: digitalWrite(ledCozinha, valor);
+      break;
+    case 3: 
+      break;
+    case 2: 
+      break;
+    case 1: 
+      break;
+  }
+}
+
+void updateQuarto(int valor, Sensor sensor, int id, const char* tipoSensor, const char* nomeComodo){
+   switch(sensor){
+    case 8: 
+      break;
+    case 7: 
+      break;
+    case 6: 
+      break;
+    case 5:
+      break;
+    case 4: digitalWrite(ledQuarto, valor);
+      break;
+    case 3: 
+      break;
+    case 2: 
+      break;
+    case 1: 
+      break;
+  }
+}
+
+void updateSala(int valor, Sensor sensor, int id, const char* tipoSensor, const char* nomeComodo){
+   switch(sensor){
+    case 4: digitalWrite(ledSala, valor);
+      break;
+    case 2: 
+      break;
+    case 0: pushData(tipoSensor, id, digitalRead(pinoPIR), nomeComodo);
+      break;
+  }
+}
+
+
+Comodo getComodoEnum(const char* nomeComodo) {
+    if (strcmp(nomeComodo, "sala") == 0) return SALA;
+    if (strcmp(nomeComodo, "quarto") == 0) return QUARTO;
+    if (strcmp(nomeComodo, "cozinha") == 0) return COZINHA;
+    if(strcmp(nomeComodo, "banheiro") == 0) return BANHEIRO;
+    return DESCONHECIDO;
+}
+
+Sensor getSensoresEnum(const char* sensor){
+  if(strcmp(sensor, "umidade") == 0) return UMIDADE;
+  if(strcmp(sensor, "presenca") ==  0) return PRESENCA;
+  if(strcmp(sensor, "distancia") == 0) return DISTANCIA;
+  if(strcmp(sensor, "led") == 0) return LED;
+  if(strcmp(sensor, "motor") == 0) return MOTOR;
+  if(strcmp(sensor, "lcd") == 0) return LCD;
+  if(strcmp(sensor, "luz") == 0) return LUZ;
+  if(strcmp(sensor, "temperatura") == 0) return TEMPERATURA;
+  if(strcmp(sensor, "led_rgb")) return LED_RGB;
+}
+
+void configureJson() {
+  String json = R"({
+  "comodos": {
+    "sala": {
+      "sensores": {
+        "presenca": {"id": 1, "valor": 1},
+        "luz": {"id": 2, "valor": 80},
+        "led": {"id": 3, "valor": 0}
+      }
+    },
+    "banheiro": {
+      "sensores": {
+        "luz": {"id": 4, "valor": 60},
+        "led": {"id": 5, "valor": 0}
+      }
+    },
+    "cozinha": {
+      "sensores": {
+        "umidade": {"id": 6, "valor": 50},
+        "luz": {"id": 7, "valor": 90},
+        "temperatura": {"id": 8, "valor": 30},
+        "led": {"id": 9, "valor": 0}
+      }
+    },
+    "quarto": {
+      "sensores": {
+        "umidade": {"id": 10, "valor": 40},
+        "luz": {"id": 11, "valor": 75},
+        "temperatura": {"id": 12, "valor": 30},
+        "led": {"id": 13, "valor": 0},
+        "lcd": {"id": 14, "valor": 1, "msg": 0},
+        "motor": {"id": 15, "valor": 0},
+        "led_rgb": {"id": 16, "r": 0, "g": 0, "b": 0}
+      }
+    }
+  }
+}
+)";
+
+  bool status = Database.set<object_t>(aClient, "/smart_home/json", object_t(json));
+  if (status)
+    Serial.println("JSON salvo com sucesso!");
+  else
+    Serial.println("Erro ao salvar JSON.");
+}
+
+void pushData(const char* tipoSensor, int id, int valor, const char* nomeComodo){
+
+   JsonWriter writer;
+
+  // Cria o objeto JSON principal
+  object_t json;
+
+  // Cria os dados que vamos inserir dentro do objeto json
+  object_t sala;
+  writer.create(sala, "valor", cont);  // O valor da luz no sensor
+
+  // Junta o objeto sala com o objeto json
+  writer.join(json, 1, sala);  // Associa sala ao json
+
+  // Envia o objeto json para o Firebase
+  if (Database.update<object_t>(aClient, "/smart_home/json/comodos/sala/sensores/luz", json)) {
+    Serial.println("Dados de luz enviados com sucesso!");
+  } else {
+   // Serial.println("Erro ao enviar dados de luz: " + firebaseData.errorReason());
+  }
+   //if(cachePresenca != valor){
+    /*object_t json;
+    JsonWriter writer;
+    String caminho = String("/smart_home/json/comodos/") + nomeComodo + "/sensores/" + tipoSensor + "/valor" + cont;
+    writer.create(json, caminho, valor); 
+
+    bool sucesso = Database.update(aClient, caminho, json);
+
+    if (sucesso) {
+      Serial.println("Dados enviados com sucesso!" + valor);
+    }
+   cachePresenca = valor;
+   //}
+  // digitalWrite(2, valor);*/
+  cont++;
+}
+
