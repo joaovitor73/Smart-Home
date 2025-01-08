a7b8b7a7fa5e41d679af7c9e1173b69e7288398c
FellipePatrick <fellipe.brito@ifrn.edu.br>
2025-01-08 15:09:03 -0300

    versão com o geolocator funcionando

--- app/lib/services/geolocator_service.dart
+++ app/lib/services/geolocator_service.dart
@@ -5,12 +5,15 @@ import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 
 class GeoLocatorService extends ChangeNotifier {
-  String latitudeCasaX1 = "-5.8950000";
-  String latitudeCasaX2 = "-5.8950000";
-  String longitudeCasaY1 = "-35.630000";
-  String longitudeCasaY2 = "-35.630000";
-  String isPresent = ""; // Estado atual (Presente ou Ausente)
-  String valorAnterior = ""; // Estado anterior
+  String isPresent = "";
+  String valorAnterior = "";
+
+  // Coordenadas da sua casa
+  final double homeLatitude = -5.885995;
+  final double homeLongitude = -35.363265;
+  final double homeRadius = 5.0; // Raio de 5 metros em torno da casa
+
+  Position? lastKnownPosition;
 
   GeoLocatorService() {
     requestPermission();
@@ -45,6 +48,7 @@ class GeoLocatorService extends ChangeNotifier {
     Geolocator.getPositionStream(
       locationSettings: LocationSettings(
         accuracy: LocationAccuracy.high,
+        distanceFilter: 0, // Força a atualização para cada mudança mínima
       ),
     ).listen((Position position) {
       captureLocation(position);
@@ -52,17 +56,31 @@ class GeoLocatorService extends ChangeNotifier {
   }
 
   Future<void> captureLocation(Position position) async {
-    // Verificar se o usuário está em casa
+    if (lastKnownPosition != null) {
+      double distance = Geolocator.distanceBetween(
+        lastKnownPosition!.latitude,
+        lastKnownPosition!.longitude,
+        position.latitude,
+        position.longitude,
+      );
+
+      if (distance < 1.0) {
+        // Ignora pequenas mudanças (menos de 1 metro)
+        return;
+      }
+    }
+
+    lastKnownPosition = position;
+
     bool estaEmCasa = _verificarPresenca(
       position.latitude,
       position.longitude,
     );
 
-    // Atualizar estado apenas se houver mudança
     if (estaEmCasa && isPresent != "Presente") {
-      ativarPresencao(true); // Usuário entrou em casa
+      ativarPresencao(true);
     } else if (!estaEmCasa && isPresent != "Ausente") {
-      ativarPresencao(false); // Usuário saiu de casa
+      ativarPresencao(false);
     }
 
     print("Posição: ${position.latitude}, ${position.longitude}");
@@ -70,8 +88,16 @@ class GeoLocatorService extends ChangeNotifier {
   }
 
   bool _verificarPresenca(double latitude, double longitude) {
-    // Lógica para verificar se está dentro das coordenadas da casa
-    return (true);
+    // Calcula a distância entre a posição atual e a posição da casa
+    double distance = Geolocator.distanceBetween(
+      homeLatitude,
+      homeLongitude,
+      latitude,
+      longitude,
+    );
+
+    // Retorna true se estiver dentro do raio de 5 metros
+    return distance <= homeRadius;
   }
 
   void ativarPresencao(bool flag) {
